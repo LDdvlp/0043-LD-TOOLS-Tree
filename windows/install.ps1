7012b1d07fbee08e88f8d412ea6491ea0cc3ef7c
LDdvlp <loicdrouet@gmail.com>
2026-07-15 17:21:21 +0200

    feat(windows): define registry root

--- windows/install.ps1
+++ windows/install.ps1
@@ -5,6 +5,7 @@ $Version = Get-Content (Join-Path $RootPath "VERSION")
 $ScriptsPath = Join-Path $RootPath "windows\scripts"
 $CreateScript = Join-Path $ScriptsPath "create-tree.cmd"
 $ExportScript = Join-Path $ScriptsPath "export-tree.cmd"
+$RegistryRoot = "HKCU:\Software\Classes\Directory\Background\shell\LD-TOOLS-Tree"
 
 Write-Host "Installing LD-TOOLS-Tree $Version..."
 Write-Host ""
